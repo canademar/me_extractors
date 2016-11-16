91355f657d7f9668554483d84aaa5d9aa5ab9710
cnavarro <cnavarro@paradigmadigital.com>
2016-11-16 12:32:55 +0100

    Added new configurations

--- projectManager/small_orchestrator.py
+++ projectManager/small_orchestrator.py
@@ -2,13 +2,13 @@
 import glob, os
 import subprocess
 from datetime import date, timedelta,datetime
-JAR_FILE = "DockerSparkPipeline-assembly-0.8.jar"
-MAIN_FILE = "orchestrator.FutureOrchestrator"
+JAR_FILE = "MixedEmotionsExampleOrchestrator-assembly-0.14.jar"
+MAIN_FILE = "orchestrator.ListFutureOrchestrator"
 CONF_FILE = "conf/textPipeline.conf"
 INPUTS_FOLDER  = "/var/data/inputs/projects/"
 
 def main():
-    output = open("log_small_orchestrator.log", "a")
+    output = open("log/log_small_orchestrator.log", "a")
     print "Starting small orchestrator"
     output.write("[%s]: Starting small orchestrator\n" % now())
     projects = [1,2,3,4,5,8,9,10]
@@ -16,7 +16,7 @@ def main():
     yesterday = today - timedelta(1)
     datestr = yesterday.strftime("%Y-%m-%d")
     for project in projects:
-        directory = "%s%s/%s/twitter/" % (root_folder, datestr, project)
+        directory = "%s%s/%s/twitter/" % (INPUTS_FOLDER, datestr, project)
         input_files = get_input_files(directory)
         for input_file in input_files:
             command = "java -cp %s %s %s %s" % (JAR_FILE, MAIN_FILE, CONF_FILE, input_file)
@@ -31,12 +31,13 @@ def main():
     output.write("[%s]: Finished\n"% now())
 
 def get_input_files(directory):
-    os.chdir(directory)
-    return ["%s%s" %(directory, file) for file in glob.glob("*.txt")]
+    filenames_array = [ filenames for root, dirnames, filenames in os.walk(directory)]
+    files  = [val for sublist in filenames_array for val in sublist]
+    return ["%s%s" %(directory, file) for file in files if file.endswith(".txt")]
 
 def now():
-    #return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    print "Now"
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    
 
 if __name__ == "__main__":
     main()
