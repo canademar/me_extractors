a8dce13ad109730868532ca0e6235eafdfaa1479
cnavarro <cnavarro@paradigmadigital.com>
2017-04-28 10:44:01 +0200

    Updated project manager configurations

---
+++ projectManager/small_orchestrator3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import glob, os
+import subprocess
+from datetime import date, timedelta,datetime
+JAR_FILE = "MixedEmotionsOrchestrator-assembly-1.0.jar"
+CONF_FILE = "conf/textPipeline.conf"
+INPUTS_FOLDER  = "/var/data/inputs/projects/"
+
+def main():
+    output = open("log/log_small_orchestrator3.log", "a")
+    print "Starting small orchestrator"
+    output.write("[%s]: Starting small orchestrator\n" % now())
+    projects = [1,2,3,4,5,8,9,10]
+    today = date.today()
+    yesterday = today - timedelta(3)
+    datestr = yesterday.strftime("%Y-%m-%d")
+    for project in projects:
+        directory = "%s%s/%s/twitter/" % (INPUTS_FOLDER, datestr, project)
+        input_files = get_input_files(directory)
+        for input_file in input_files:
+            command = "java -jar %s %s %s" % (JAR_FILE, CONF_FILE, input_file)
+            print "Executing '%s'" % command
+            output.write("[%s]: Execution '%s'\n" % (now(), command))
+            code = subprocess.call(command.split(" "))
+            if code == 0:
+                output.write("[%s]: Success!\n"% now())
+            else:
+                output.write("[%s]: Error!!!!!\n"% now())
+    print "----small orchestrator finished"
+    output.write("[%s]: Finished\n"% now())
+
+def get_input_files(directory):
+    filenames_array = [ filenames for root, dirnames, filenames in os.walk(directory)]
+    files  = [val for sublist in filenames_array for val in sublist]
+    return ["%s%s" %(directory, file) for file in files if file.endswith(".txt")]
+
+def now():
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    
+
+if __name__ == "__main__":
+    main()
